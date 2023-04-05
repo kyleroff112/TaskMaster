d9ad07e8d321d33d4397708d9fdd4c1dfb7f2206
kyleroff112 <kyleroff112@gmail.com>
2023-04-04 22:12:00 -0400

    added stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/taskapp', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -26,7 +26,7 @@ db.once('open', () => {
 app.use('/api/users', userRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
